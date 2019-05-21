e13619437cf657ffe762a36449d319fadc0c33fb
Mlungisi DUBE <mdube@c4r23s1.wethinkcode.co.za>
2019-05-21 11:26:17 +0200

    ft_strdup file

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,33 +6,29 @@
 /*   By: mdube <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 15:56:12 by mdube             #+#    #+#             */
-/*   Updated: 2019/05/20 16:55:41 by mdube            ###   ########.fr       */
+/*   Updated: 2019/05/21 11:21:06 by mdube            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <string.h>
 #include <stdlib.h>
 
 char		*ft_strdup(char *src)
 {
-	int i;
-	char *dst;
+	int		i;
+	char	*dst;
 
 	i = 0;
-	while(src[i])
+	while (src[i])
 		i++;
-
-	if(src == NULL)
-		return NULL;
-	else
-		dst = (char*)malloc(sizeof(char) * (i + 1));
-
+	if (!(dst = (char*)malloc(sizeof(char) * (i + 1))))
+		return (NULL);
 	i = 0;
-	while(src[i])
+	while (src[i])
 	{
 		dst[i] = src[i];
 		i++;
 	}
-
 	dst[i] = '\0';
 	return (dst);
 }
