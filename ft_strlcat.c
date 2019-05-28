3a0d67b2e5016edc50e0528c31adad2b5561282f
Mlungisi DUBE <mdube@c4r23s1.wethinkcode.co.za>
2019-05-28 11:18:04 +0200

    ft_strlcat file

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,46 +6,31 @@
 /*   By: mdube <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 16:54:52 by mdube             #+#    #+#             */
-/*   Updated: 2019/05/27 17:20:10 by mdube            ###   ########.fr       */
+/*   Updated: 2019/05/28 11:17:11 by mdube            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <stdio.h>
-#include <string.h>
-#include <string.h>
 
-char		*ft_strlcat(char *dst, const char *src)
+size_t		ft_strlcat(char *dst, const char *src, size_t n)
 {
-	int i;
-	int j;
-	//size_t len;
+	int		i;
+	int		j;
+	size_t	len;
 
 	i = 0;
 	j = 0;
-	while (dst[i])
-		i++;
-	while (src[j])
+	if (n == 0)
 	{
-		dst[i] = src[j];
-		j++;
-		i++;
-		//n--;
+		while (src[i])
+			i++;
+		return (i);
 	}
-	/*len = 0;
-	while (dst[len])
-		len++;*/
-	dst[i] = '\0';
-	return (dst);
-}
-
-int			main()
-{
-	char firstname[20] = "Mlungisi ";
-	char *lastname = "Dube";
-
-	char *mine = ft_strlcat(firstname, lastname);
-	char *computer = strcat(firstname, lastname);
-
-	printf("%s\n", computer);
-	printf("%s\n", mine);
-	return 0;
+	while (src[i])
+		i++;
+	while (dst[j] && 0 < n)
+	{
+		j++;
+		n--;
+	}
+	len = j + i;
+	return (len);
 }
