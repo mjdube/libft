fcd73a8b80b255a4dbaecc18b727224b42ceddab
Mlungisi DUBE <mdube@c4r23s1.wethinkcode.co.za>
2019-05-31 17:24:30 +0200

    new files

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -1,29 +1,62 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*   ft_strsplit.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mdube <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/05/28 11:25:25 by mdube             #+#    #+#             */
-/*   Updated: 2019/05/31 11:52:56 by mdube            ###   ########.fr       */
+/*   Created: 2019/05/31 11:48:08 by mdube             #+#    #+#             */
+/*   Updated: 2019/05/31 17:23:22 by mdube            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
-char		*ft_itoa(int n)
+char **ft_strsplit(const char *s, char c)
 {
-	
+	char **str;
+	int i;
+	int j;
+	int k;
+
+	i = 0;
+	j = 0;
+	k = 0;
+	str = (char **)malloc(sizeof(char *) * strlen(s));
+	while (s[i])
+	{
+		if (s[i] == c)
+		{
+			i++;
+		}
+		else
+		{
+			while (s[i] != c)
+			{
+				str[j][k] = s[i];
+				i++;
+				k++;
+			}
+			if (s[i] == c)
+			{
+				str[j][k] = '\0';
+				j++;
+			}
+			k = 0;
+		}
+	}
+	return (str);
 }
 
 int		main()
 {
+	char *str = "*hello***fellow***students";
 
-	printf("%s", ft_itoa(2));
+	char **n = ft_strsplit(str, '*');
+
+	printf("%s", n[1]);
 
 	return 0;
 }
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,33 +6,48 @@
 /*   By: mdube <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/30 18:00:45 by mdube             #+#    #+#             */
-/*   Updated: 2019/05/30 18:17:52 by mdube            ###   ########.fr       */
+/*   Updated: 2019/05/31 13:48:12 by mdube            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdlib.h>
+#include <string.h>
+
+int		ft_isspace(char c)
+{
+	int i;
+
+	if (c == '\n' || c == '\v' || c  == '\f' || c == ' ' || c == '\r' || c == '\t')
+		i = 1;
+	else
+		i = 0;
+	return (i);
+}
 
 char 		*ft_strtrim(const char *s)
 {
 	char *new;
 	int i;
+	int j;
+	int k;
 
 	i = 0;
 	new = (char *)malloc(sizeof(char) * strlen(s) + 1);
+	if (new == NULL)
+		return (NULL);
+	while (ft_isspace(s[i]) == 1)
+		i++;
+	k = 0;
 	while (s[i])
 	{
-		if (ft_isspace(s[i]))
+		new[k] = s[i];
+		k++;
+		i++;
 	}
-}
-
-int			ft_isspace(char c)
-{
-	int i;
-
-	if (c == '\t' || c == '\n' || c == '\v' || c == '\f' || c == '\r' || c == ' ')
-		i = 1;
-	else 
-		i = 0;
-	return (i);
+	while (ft_isspace(new[k - 1]) == 1)
+	{
+		k--;
+	}
+	new[k] = '\0';
+	return (new);
 }
