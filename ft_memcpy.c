7d0a0a5a8e691734e18965c05d8962adeadeb9a6
Mlungisi DUBE <mdube@c4r13s7.wethinkcode.co.za>
2019-06-05 18:38:18 +0200

    fix

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,11 +6,12 @@
 /*   By: mdube <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/24 12:19:32 by mdube             #+#    #+#             */
-/*   Updated: 2019/06/05 16:21:15 by mdube            ###   ########.fr       */
+/*   Updated: 2019/06/05 18:37:48 by mdube            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
+#include "libft.h"
 
 void				*ft_memcpy(void *dst, const void *src, size_t n)
 {
@@ -20,17 +21,30 @@ void				*ft_memcpy(void *dst, const void *src, size_t n)
 
 	if (dst == NULL && src == NULL)
 		return (NULL);
-	else
+	i = 0;
+	d = (char *)dst;
+	s = (const char *)src;
+/*	while (i < n)
 	{
-		i = 0;
-		d = (char *)dst;
-		s = (const char *)src;
-		while (s[i] && i < n)
+		d[i] = s[i];
+		i++;
+	}
+//	d[i] = '\0';*/
+	if ((int)n < ft_strlen(s))
+	{
+		while (i < n)
 		{
 			d[i] = s[i];
 			i++;
 		}
-		d[i] = '\0';
+	}
+	else
+	{
+		while (s[i])
+		{
+			d[i] = s[i];
+			i++;
+		}
 	}
 	return (dst);
 }
