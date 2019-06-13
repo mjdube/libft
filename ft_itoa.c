74a02e8a4ecab6cf68a64df103cc4566495a81ca
mjdube <50707269+mjdube@users.noreply.github.com>
2019-06-13 05:46:50 +0200

    13th June 2019

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,25 +6,51 @@
 /*   By: mdube <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/28 11:25:25 by mdube             #+#    #+#             */
-/*   Updated: 2019/06/12 17:52:32 by mdube            ###   ########.fr       */
+/*   Updated: 2019/06/05 17:11:00 by mdube            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
 #include <stdlib.h>
-#include <stdio.h>
 #include <string.h>
 
-char				*ft_itoa(int n)
+static int	get_nb_size(unsigned int nb)
 {
-	char			*str;
-	unsigned int	nbr;
-	unsigned int	i;
+	unsigned int size;
+
+	size = 0;
+	while (nb >= 10)
+	{
+		nb /= 10;
+		++size;
+	}
+	return (size + 1);
+}
+
+char			*ft_itoa(int n)
+{
+
+	char		*str;
+	unsigned int	nb;
+	unsigned int	index;
 	unsigned int	size;
 
-	if (nbr < 0)
-		n = (unsigned int)(nbr * -1);
+	if (n < 0)
+		nb = (unsigned int)(n * -1);
 	else
-		n = (unsigned int)nbr;
-
+		nb = (unsigned int)n;
+	size = (unsigned int)get_nb_size(nb);
+	index = 0;
+	if (!(str = (char *)malloc(sizeof(char) * (size + 1 + (n < 0 ? 1 : 0)))))
+		return (0);
+	if (n < 0 && (str[index] = '-'))
+		size++;
+	index = size - 1;
+	while (nb >= 10)
+	{
+		str[index--] = (char)(nb % 10 + 48);
+		nb /= 10;
 	}
+	str[index] = (char)(nb % 10 + 48);
+	str[size] = '\0';
+	return (str);	
+}
