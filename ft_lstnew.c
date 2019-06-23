6158b6340e55c99048b6081a938187b38141a64d
Mlungisi DUBE <mdube@c4r13s7.wethinkcode.co.za>
2019-06-23 16:27:10 +0200

    23rd June 2019

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,13 +6,12 @@
 /*   By: mdube <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/23 13:11:26 by mdube             #+#    #+#             */
-/*   Updated: 2019/06/23 15:40:27 by mdube            ###   ########.fr       */
+/*   Updated: 2019/06/23 16:22:22 by mdube            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <string.h>
-
 
 t_list		*ft_lstnew(void const *content, size_t content_size)
 {
@@ -27,8 +26,7 @@ t_list		*ft_lstnew(void const *content, size_t content_size)
 	}
 	else
 	{
-		if (!((fresh = ft_memcpy(fresh->content, content, content_size)) == NULL))
-			return (NULL);
+		fresh->content = (void *)content;
 		fresh->content_size = content_size;
 	}
 	fresh->next = NULL;
